8b857fb5eb9b70042aa0e16b93f41574920bdd6e
Rib1356 <rib1356@hotmail.co.uk>
2019-06-07 14:08:52 +0100

    Added the ability to create a new plant and groups

---
+++ PlantService/DTO/GroupDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlantService.DTO
+{
+    public class NewPlantDTO
+    {
+        public string Name { get; set; }
+        public string Sku { get; set; }
+        public IEnumerable<GroupDTO> GroupDetails { get; set; }
+    }
+
+    public class GroupDTO
+    {
+        public int GroupId { get; set; }
+        public string GroupDescription { get; set; }
+
+    }
+}
