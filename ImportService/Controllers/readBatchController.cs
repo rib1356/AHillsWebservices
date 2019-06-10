0da27c4e8e6970b68a8163e2bdb6153a4a28cfef
Barry Hebbron <B.D.Hebbron@tees.ac.uk>
2019-06-10 18:30:49 +0100

    Refactored Test client - needs real server uri

--- ImportService/Controllers/readBatchController.cs
+++ ImportService/Controllers/readBatchController.cs
@@ -13,8 +13,28 @@ namespace ImportService.Controllers
         // GET: readBatch
         public ActionResult Index()
         {
-            var batches = new List<DTO.BatchDTO>().AsEnumerable(); 
+            // empty batches object to fill soon
+            var batches = new List<DTO.BatchDTO>().AsEnumerable();
+            // build a client to chat to the web service
             HttpClient client = ApiClient();
+            // dear service can i have the batches please
+            batches = GetBatches(batches, client);
+            // transform the services into a viewModel
+            IEnumerable<DTO.BatchVM> VM = buildVM(batches);
+
+            return View(VM);
+
+
+        }
+
+        /// <summary>
+        /// ASk our Client for the batches please
+        /// </summary>
+        /// <param name="batches"></param>
+        /// <param name="client"></param>
+        /// <returns>IEnumerable<DTO.BatchDTO></returns>
+        private static IEnumerable<DTO.BatchDTO> GetBatches(IEnumerable<DTO.BatchDTO> batches, HttpClient client)
+        {
             HttpResponseMessage response = client.GetAsync("api/Batches/All").Result;
             if (response.IsSuccessStatusCode)
             {
@@ -24,17 +44,26 @@ namespace ImportService.Controllers
             {
                 Debug.WriteLine("Index received a bad response from the web service.");
             }
-            IEnumerable<DTO.BatchVM> VM = batches.Select(b => new DTO.BatchVM
+
+            return batches;
+        }
+
+
+        /// <summary>
+        /// Builds a VM
+        /// </summary>
+        /// <param name="batches"></param>
+        /// <returns></returns>
+        private static IEnumerable<DTO.BatchVM> buildVM(IEnumerable<DTO.BatchDTO> batches)
+        {
+            return batches.Select(b => new DTO.BatchVM
             {
-                 Sku = b.Sku,
-                  Name = b.Name,
-                   FormSize = b.FormSize,
-                    Quantity = b.Quantity,
-                     WholesalePrice = b.WholesalePrice
+                Sku = b.Sku,
+                Name = b.Name,
+                FormSize = b.FormSize,
+                Quantity = b.Quantity,
+                WholesalePrice = b.WholesalePrice
             }).AsEnumerable();
-            return View(VM);
-
-
         }
 
 
