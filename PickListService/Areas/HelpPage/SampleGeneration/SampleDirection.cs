f40c42081aca845a0933d5779b8988189adec1a7
Rib1356 <rib1356@hotmail.co.uk>
2019-10-14 17:14:33 +0100

    Revert "Revert "Picklist Services and other stuff""
    This reverts commit 8a5b28429bc7910f19bf4126da3de2ad742a0074.

---
+++ PickListService/Areas/HelpPage/SampleGeneration/SampleDirection.cs
@@ -0,0 +1,11 @@
+namespace PickListService.Areas.HelpPage
+{
+    /// <summary>
+    /// Indicates whether the sample is used for request or response
+    /// </summary>
+    public enum SampleDirection
+    {
+        Request = 0,
+        Response
+    }
+}
